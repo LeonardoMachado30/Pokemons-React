0dae5d0c2c974909821d94cd82a9a395d374ee47
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2023-02-07 13:54:23 -0300

    Updates

--- _next/static/L9rIVCOFTa4MAnA4b4_V0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/805-e6432c5334ddc144.js","static/chunks/pages/index-9caf7940df5dafc8.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
